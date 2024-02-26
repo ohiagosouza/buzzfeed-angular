77be91bf5d72f1fef382ab6d131b13272ef69500
Hiago <hiago@Hiagos-MacBook-Pro.local>
2024-02-26 19:28:58 -0300

    add app config file & quizz functionalities

--- src/app/component/quizz/quizz.component.ts
+++ src/app/component/quizz/quizz.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.css'
+  styleUrl: './quizz.component.css',
 })
 export class QuizzComponent {
+  playerChoice(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
 
+  nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      this.finished = true;
+    }
+  }
+
+  title: string = '';
+
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = true;
+
+  constructor() {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
 }
